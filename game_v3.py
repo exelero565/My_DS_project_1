563ad6b9a1218bd15b07aa5991ee086a6db7a8ca
exelero565 <veyron565@yandex.ru>
2022-02-03 18:25:43 +0300

    fix code

--- game_v3.py
+++ game_v3.py
@@ -2,8 +2,10 @@
 Компьютер сам загадывает и сам угадывает число
 """
 
+import numpy as np
 
-def game_core_v3(number):
+
+def random_predict(number: int = 1) -> int:
     predict = 50
     count = 1
     if predict < number:
@@ -47,5 +49,29 @@ def game_core_v3(number):
         predict -= number
         count += 1
     return count
- 
-print(max(map(game_core_v3, range(1, 101))))
+
+
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
